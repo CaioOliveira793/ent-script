7ebd9f196f5d18293b3b7e7e29f2f4b06ac2a67b
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-10-31 15:22:39 -0300

    test: insert components in non existent entities

--- test/Component.test.ts
+++ test/Component.test.ts
@@ -1,6 +1,8 @@
 import PropertyType from '../src/PropertyTypes';
 import Storage, { ComponentSchema, PoolSettings } from '../src/Storage';
 
+const POOL_INCREASE_COUNT = 5;
+
 interface ComponentData {
 	property: number;
 }
@@ -9,7 +11,7 @@ class Component implements ComponentData {
 	public property: number = 2;
 
 	public static schema: ComponentSchema = { property: PropertyType.BYTE };
-	public static poolSettings: PoolSettings = { initialCount: 0, increaseCount: 10 };
+	public static poolSettings: PoolSettings = { initialCount: 0, increaseCount: POOL_INCREASE_COUNT };
 }
 
 describe('Component', () => {
@@ -19,10 +21,23 @@ describe('Component', () => {
 		const entity = storage.create();
 
 		const component = storage.insert<Component>(entity, Component);
-		console.log(component);
 		expect(component).toStrictEqual({
 			property: 2
 		});
+	});
+
+	it('throw an error when insert a component in a non created entity', () => {
+		const storage = new Storage([Component]);
+
+		expect(() => storage.insert<Component>(3, Component)).toThrowError('can not insert a component in a non crated entity');
+	});
+
+	it('throw an error when insert a component in a deleted entity', () => {
+		const storage = new Storage([Component]);
+		const entity = storage.create();
+		storage.destroy(entity);
+
+		expect(() => storage.insert<Component>(entity, Component)).toThrowError('can not insert a component in a non crated entity');
 	});
 
 });
