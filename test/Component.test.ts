9b17d9eeaf63ce91aabe18516caaa3ed0d12ea28
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-11-04 19:48:03 -0300

    feat: maps properties types

--- test/Component.test.ts
+++ test/Component.test.ts
@@ -16,6 +16,37 @@ class Component {
 	};
 }
 
+class FullPropertyComponent {
+	public u_int_8: number = 250;
+	public u_int_16: number = 65000;
+	public u_int_32: number = 4200000000;
+	public u_int_64: bigint = BigInt(8446744073709551615);
+
+	public int_8: number = -120;
+	public int_16: number = -32000;
+	public int_32: number = -2100000000;
+	public int_64: bigint = BigInt(-844674407370955);
+
+	public float_32: number = 3.141592653589793238;
+	public float_64: number = 3.14159265358979323846264338327950288;
+
+	public static poolSettings: PoolSettings = { initialCount: 0, increaseCount: POOL_INCREASE_COUNT };
+	public static schema: ComponentSchema = {
+		u_int_8: PropertyType.U_INT_8,
+		u_int_16: PropertyType.U_INT_16,
+		u_int_32: PropertyType.U_INT_32,
+		u_int_64: PropertyType.U_INT_64,
+
+		int_8: PropertyType.INT_8,
+		int_16: PropertyType.INT_16,
+		int_32: PropertyType.INT_32,
+		int_64: PropertyType.INT_64,
+
+		float_32: PropertyType.FLOAT_32,
+		float_64: PropertyType.FLOAT_64,
+	};
+}
+
 
 describe('Component insertion', () => {
 
@@ -148,6 +179,25 @@ describe('Component return', () => {
 		compRef.prop1 = 250;
 
 		expect(storage.retrieve<Component>(entity, Component)).toStrictEqual(compRef);
+	});
+
+	it('maps properties to return in component reference', () => {
+		const storage = new Storage([FullPropertyComponent]);
+		const entity = storage.create();
+		storage.insert<FullPropertyComponent>(entity, FullPropertyComponent);
+
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).u_int_8).toBe(250);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).u_int_16).toBe(65000);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).u_int_32).toBe(4200000000);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).u_int_64).toBe(BigInt(8446744073709551615));
+
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).int_8).toBe(-120);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).int_16).toBe(-32000);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).int_32).toBe(-2100000000);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).int_64).toBe(BigInt(-844674407370955));
+
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).float_32).toBeGreaterThanOrEqual(3.141592653589793238);
+		expect(storage.retrieve<FullPropertyComponent>(entity, FullPropertyComponent).float_64).toBeGreaterThanOrEqual(3.14159265358979323846264338327950288);
 	});
 
 });
